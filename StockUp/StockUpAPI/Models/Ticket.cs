2e92297858cfa2fc35922eb385a10de09bc8b70d
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-11 18:39:04 -0400

    Added basic models with attributes for User and Ticket

--- StockUp/StockUpAPI/Models/Ticket.cs
+++ StockUp/StockUpAPI/Models/Ticket.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 namespace StockUpAPI.Models
 {
     public class Ticket
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Game { get; set; }
+
+        [Required]
+        public int Pack { get; set; }
+
+        [Required]
+        public int Nbr { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public String Name { get; set; }
+        public String Store { get; set; }
+        public int Activated { get; set; } // tiny int 0 1
+        public int LoadedPack { get; set; } // tiny int 0 1
+        public int Price { get; set; }
+        public int Start_Inv { get; set; } // tiny int 0 1 
+        public int End_Inv { get; set; } // tiny int 0 1
+        public String Emp_id { get; set; }
+
         public Ticket()
         {
         }
---
+++ StockUp/StockUpAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockUpAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public String Empy_id { get; set; }
+
+        public String First { get; set; }
+        public String Last { get; set; }
+        public String Email { get; set; }
+        public int IsAdmin { get; set; } // tiny int 0 1
+
+        public User()
+        {
+        }
+    }
+}
